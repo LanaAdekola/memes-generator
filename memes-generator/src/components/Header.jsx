1875a2d147c8bd9e35d69316c6d787e7f65ab402
LanaAdekola <adekolaadelana@gmail.com>
2023-05-04 16:12:04 +0100

    .

--- memes-generator/src/components/Header.jsx
+++ memes-generator/src/components/Header.jsx
@@ -4,11 +4,11 @@ export default function Header() {
     return (
         <header className="header">
             <img 
-                src="./images/troll-face.png" 
+                src="./src/assets/troll-face.png" 
                 className="header--image"
             />
             <h2 className="header--title">Meme Generator</h2>
-            <h4 className="header--project">React Course - Project 3</h4>
+            <h4 className="header--project">Adelana's - Project 3</h4>
         </header>
     )
 }
